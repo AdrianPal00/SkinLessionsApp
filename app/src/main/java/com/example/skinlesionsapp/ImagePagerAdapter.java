6d50a2816dda5d0f5d1f16eb433fd447b2a92e25
Adrian <adrian.pal00@e-uvt.ro>
2023-08-04 15:45:23 +0300

    Skin Lesions Classification App -> use camera / use gallery. Initial Description.

---
+++ app/src/main/java/com/example/skinlesionsapp/ImagePagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.skinlesionsapp;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImagePagerAdapter extends RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder> {
+
+    private List<Bitmap> images;
+
+    public ImagePagerAdapter() {
+        images = new ArrayList<>();
+    }
+
+    public void addImage(Bitmap imageBitmap) {
+        images.add(imageBitmap);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image, parent, false);
+        return new ImageViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        Bitmap imageBitmap = images.get(position);
+        holder.imageView.setImageBitmap(imageBitmap);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/skinlesionsapp/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.skinlesionsapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.skinlesionsapp.R;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1001;
+    private static final int REQUEST_CAMERA = 2001;
+    private static final int REQUEST_GALLERY = 2002;
+
+    private TextView descriptionTextView;
+    private Button buttonCamera;
+    private Button buttonGallery;
+    private ImageView imageView;
+    private LinearLayout lesionTypesLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        descriptionTextView = findViewById(R.id.descriptionTextView);
+        imageView = findViewById(R.id.imageView);
+        buttonCamera = findViewById(R.id.buttonCamera);
+        buttonGallery = findViewById(R.id.buttonGallery);
+        lesionTypesLayout = findViewById(R.id.lesionTypesLayout);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkCameraPermission()) {
+                    openCamera();
+                }
+                lesionTypesLayout.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+                lesionTypesLayout.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private boolean checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, REQUEST_CAMERA);
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA && data != null) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                if (photo != null) {
+                    imageView.setImageBitmap(photo);
+                    imageView.setVisibility(View.VISIBLE);
+                    descriptionTextView.setVisibility(View.GONE);
+                } else {
+                    Toast.makeText(this, "Failed to capture image", Toast.LENGTH_SHORT).show();
+                }
+            } else if (requestCode == REQUEST_GALLERY && data != null) {
+                Uri imageUri = data.getData();
+                Bitmap selectedImage = getBitmapFromUri(imageUri);
+                if (selectedImage != null) {
+                    imageView.setImageBitmap(selectedImage);
+                    imageView.setVisibility(View.VISIBLE);
+                    descriptionTextView.setVisibility(View.GONE);
+                } else {
+                    Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    private Bitmap getBitmapFromUri(Uri uri) {
+        try {
+            return MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+}
